38d6496df21d32e33929a0add8473e37fb1bee8f
subin <han04245@naver.com>
2025-10-20 19:00:12 +0900

    fix(InputView): 여러 줄 입력 받을 수 있도록 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -8,6 +8,10 @@ public class InputView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        if (input.startsWith("//") && !input.contains("\n") && sc.hasNextLine()) {
+            String second = sc.nextLine();
+            input = input + "\n" + second;  // 실제 개행 삽입
+        }
         return input.trim();
     }
 }
