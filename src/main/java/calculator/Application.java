7d98c6aaa6774e5d53a3008cadc54014afda87c9
subin <han04245@naver.com>
2025-10-20 18:35:43 +0900

    feat(OutputView): 결과 출력
    + 테스트 코드 수정(\\n -> \n)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,13 @@
 package calculator;
 
 import calculator.controller.CalculatorController;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         CalculatorController calculator = new CalculatorController();
         calculator.run();
+        OutputView outputView = new OutputView();
+        outputView.printOutput(123);
     }
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+
+    public void printOutput(int result) {
+        System.out.println("결과 : " + result);
+    }
+}
+
