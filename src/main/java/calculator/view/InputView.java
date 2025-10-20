d597669e61638724b494de9112acc60094f78b92
subin <han04245@naver.com>
2025-10-20 21:57:41 +0900

    fix: 테스트 코드 복구 및 InputView 코드 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,14 +3,12 @@ package calculator.view;
 import java.util.Scanner;
 
 public class InputView {
-
-    public String readInput(){
+    public String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        if (input.startsWith("//") && !input.contains("\n") && sc.hasNextLine()) {
-            String second = sc.nextLine();
-            input = input + "\n" + second;  // 실제 개행 삽입
+        if (input.contains("\\n")) {
+            input = input.replace("\\n", "\n");
         }
         return input.trim();
     }
